81f9c7ed7b3195070e92bc0e71d9454e070e42d3
Karpagavalli-m <karpagavallim0401@gmail.com>
2024-11-22 14:44:20 +0530

    useAxiosFetch.js

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -37,7 +37,7 @@ const useAxiosFetch = (dataUrl) => {
 
             function cleanUp()
             {
-            const isMounted=false;
+            isMounted=false;
             source.cancel()
             }
          return cleanUp()
